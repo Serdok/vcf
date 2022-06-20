bb58d426655d5966df0851e7a1eccfff499bbe05
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-06-20 16:29:45 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VetCarniFood
+
+[ESTIAM] Hackathon - VetCarniFood
+
---
+++ android/app/src/main/kotlin/com/estiam/vet_carni_food/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.estiam.vet_carni_food
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
